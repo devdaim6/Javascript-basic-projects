c4f86115ecf448890f49403332b7978939711bc3
DaimZahoor <123406647+devdaim6@users.noreply.github.com>
2023-12-22 02:29:27 +0530

    Delete 9.directory

--- 9.LoveCalculator/main.js
+++
@@ -1,8 +0,0 @@
-function checkLove(){
-    setInterval(calculate(),5000);
-}
-function calculate(){
-    let random=Math.floor((Math.random()*100)+1);
-    let Value=document.getElementById("lovevalue");
-    Value.value=random;
-}
